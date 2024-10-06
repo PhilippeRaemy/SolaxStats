4dbe3cd56ad8c80ea0c08b27597d687820f52a24
philipperaemy <philippe_raemy@swissonline.ch>
2024-10-06 14:54:51 +0200

    refactor configure static class and setup basic logic for aggregate file reading and saving

--- configure.py
+++ configure.py
@@ -3,29 +3,71 @@ import json
 import os
 import re
 import sys
+from datetime import datetime
 
 import click
+
+file_segments = []
+dated_file_pattern: re.compile('.*\.(?P<year>\d{4})-(?P<month>\d\d)-(?P<day>\d\d)\..*$')
+
+
+def date_from_filename(filename):
+    ma = dated_file_pattern.match(filename)
+    if ma:
+        di = ma.groupdict()
+        return datetime(int(di['year']), int(di['month']), int(di['day']))
+    return datetime(1900, 1, 1)
+
+
+def gen_json_d(filename):
+    return '.'.join(file_segments, [date_from_filename(filename).strftime('%Y-%m-%d'), 'json']),
+
+
+def gen_feather_d(filename):
+    return '.'.join(file_segments, [date_from_filename(filename).strftime('%Y-%m-%d'), 'feather']),
+
+
+def gen_feather_m(filename):
+    return '.'.join(file_segments, [date_from_filename(filename).strftime('%Y-%m'), 'feather']),
+
+
+def gen_feather_y(filename):
+    return '.'.join(file_segments, [date_from_filename(filename).strftime('%Y'), 'feather']),
+
+
+def gen_feather_a(filename):
+    return '.'.join(file_segments, ['feather'])
 
 
 def read_config():
     solax_stats_folder = os.environ.get('SOLAX_STATS_FOLDER', os.path.dirname(__file__))
     solax_stats_file = os.environ.get('SOLAX_STATS_FILE', 'solax.json')
 
+    def insert_re(segments, *args) -> re:
+        return re.compile('.'.join(segments + args))
+
     local_file = os.path.join(solax_stats_folder, solax_stats_file)
+    file_segments = solax_stats_file.split('.')[:-1]
+
     config = {
-        'user_name': os.environ.get('USER_NAME'),
-        'site_password': os.environ.get('SITE_PASSWORD'),
-        'config_password': os.environ.get('CONFIG_PASSWORD'),
-        'encrypted_password': os.environ.get('ENCRYPTED_PASSWORD'),
-        'site_id': os.environ.get('SITE_ID'),
-        'solax_stats_folder': solax_stats_folder,
-        'solax_stats_file': solax_stats_file,
+        'user_name'          : os.environ.get('USER_NAME'),
+        'site_password'      : os.environ.get('SITE_PASSWORD'),
+        'config_password'    : os.environ.get('CONFIG_PASSWORD'),
+        'encrypted_password' : os.environ.get('ENCRYPTED_PASSWORD'),
+        'site_id'            : os.environ.get('SITE_ID'),
+        'solax_stats_folder' : solax_stats_folder,
+        'solax_stats_file'   : solax_stats_file,
         'target_file_pattern': re.compile('.*(?P<year>\d{4})-(?P<month>\d\d)-(?P<day>\d\d).json$'),
-        'local_file': local_file
+        'local_file'         : local_file,
+        're_json'            : insert_re(file_segments, r'(?P<yyyy>\d{4})-(?P<mm>\d\d)-(?P<dd>\d\d)', 'json'),
+        're_feather_d'       : insert_re(file_segments, r'(?P<yyyy>\d{4})-(?P<mm>\d\d)-(?P<dd>\d\d)', 'feather'),
+        're_feather_m'       : insert_re(file_segments, r'(?P<yyyy>\d{4})-(?P<mm>\d\d)', 'feather'),
+        're_feather_y'       : insert_re(file_segments, r'(?P<yyyy>\d{4})', 'feather'),
+        're_feather_a'       : insert_re(file_segments, 'feather'),
+        'file_segments'      : file_segments
     }
 
-
-    indirections=5
+    indirections = 5
     while --indirections:
         if os.path.exists(local_file):
             with open(local_file, 'r') as cgf:
