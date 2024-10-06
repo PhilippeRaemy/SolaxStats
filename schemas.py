31b4d364e6c3b106bfff91e3d9816fce79375381
philipperaemy <philippe_raemy@swissonline.ch>
2024-10-06 16:18:56 +0200

    WIP aggregate energies and powers in hours, days or months

--- schemas.py
+++ schemas.py
@@ -1,11 +1,14 @@
 class Schema:
-    def __init__(self, key_columns, power_column, energy_columns, info_columns):
+    def __init__(self, key_columns, power_columns, energy_columns, info_columns):
         self.key_columns = key_columns
-        self.data_columns = power_column, energy_columns, info_columns
+        self.data_columns = power_columns, energy_columns, info_columns
+        self.power_columns = power_columns
+        self.energy_columns = energy_columns
+        self.info_columns = info_columns
 
 
 POWER_SCHEMA = Schema(
-    power_column=[
+    power_columns=[
         "powerdc1",
         "powerdc2",
         "powerdc3",
@@ -22,6 +25,7 @@ POWER_SCHEMA = Schema(
         "feedinPowerMeter2",
         "relayPower",
         "batPower1",
+        "totalChargePower"
     ],
     key_columns=[
         "year",
@@ -42,7 +46,65 @@ POWER_SCHEMA = Schema(
     ],
     energy_columns=[
         "consumeEnergyMeter2",
-        "batteryCapacity",
-        "totalChargePower"]
+        "batteryCapacity"]
+
+)
+
+ENERGY_SCHEMA = Schema(
+    power_columns=[
+        "powerdc1",
+        "powerdc2",
+        "powerdc3",
+        "powerdc4",
+        "pac1",
+        "pac2",
+        "pac3",
+        "pvPower",
+        "gridpower",
+        "feedinpower",
+        "EPSPower",
+        "epspower",
+        "EpsActivePower",
+        "feedinPowerMeter2",
+        "relayPower",
+        "batPower1",
+        "totalChargePower",
+    ],
+    key_columns=[
+        "year",
+        "month",
+        "day",
+        "hour",
+        "minute",
+        "timestamp"
+    ],
+    info_columns=[
+        "inverterSn",
+        "inverterType",
+        "pileSn",
+        "fiveMinuteVal",
+        "uploadTimeValue",
+        "Meter2ComState",
+        "elapsed_time"
+    ],
+    energy_columns=[
+        "powerdc1KWh",
+        "powerdc2KWh",
+        "powerdc3KWh",
+        "powerdc4KWh",
+        "pac1KWh",
+        "pac2KWh",
+        "pac3KWh",
+        "pvPowerKWh",
+        "gridpowerKWh",
+        "feedinpowerKWh",
+        "EPSPowerKWh",
+        "epspowerKWh",
+        "EpsActivePowerKWh",
+        "feedinPowerMeter2KWh",
+        "relayPowerKWh",
+        "batPower1KWh",
+        "consumeEnergyMeter2",
+        "batteryCapacity"]
 
 )
