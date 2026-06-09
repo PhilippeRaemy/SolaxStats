ec74b14cf6ee4a5f5ac59943e7b7547737279cf5
Philippe Raemy <philippe.raemy@kochind.com>
2026-06-09 14:39:04 +0200

    fix smoke unit tests

--- tests/integration/test_extract_group_smoke.py
+++ tests/integration/test_extract_group_smoke.py
@@ -1,8 +1,10 @@
 from click.testing import CliRunner
 from solax_extract import extract
 
+
 def test_extract():
-  runner = CliRunner()
-  result = runner.invoke(extract)
-  assert result.exit_code == 0
-  print(result.output)
+    runner = CliRunner()
+    result = runner.invoke(extract, ['--help'])
+    assert result.exit_code == 0
+    assert 'history' in result.output
+    assert 'aggregate' in result.output
